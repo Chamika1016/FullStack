6d85cd14aedb34633900dd86df1ce7e73b40eb47
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-06-23 23:57:04 +0530

    add home page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 Chamamia. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          alt="chamamia facebook"
+          className={styles.icon}
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          alt="chamamia"
+          className={styles.icon}
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          alt="chamamia"
+          className={styles.icon}
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          alt="chamamia"
+          className={styles.icon}
+        />
+      </div>
     </div>
   );
 };
