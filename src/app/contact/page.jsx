c1f798fe797d0526aa21957f33c4a75cd539f1ce
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-06-24 14:28:01 +0530

    adding contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 const page = () => {
-  return <div className={styles.contact}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+            placeholder="message"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
