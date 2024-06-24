f302f069fb38d0e1e38f3db88d4acb7bc0d7c4b1
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-06-24 21:20:26 +0530

    adding blog page

--- src/app/blog/[blogId]/page.jsx
+++ src/app/blog/[blogId]/page.jsx
@@ -1,7 +1,50 @@
 import React from "react";
-
+import styles from "./page.module.css";
+import Image from "next/image";
 const page = () => {
-  return <div>Blog Posts</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+            quaerat veniam ab aspernatur earum laborum, molestias sunt corporis
+            nostrum dolorem nesciunt consequuntur vitae sequi, error obcaecati
+            corrupti? Accusantium, rerum officiis.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="/image2.jpg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Chamika Lakshitha</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/image4.jpg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis
+          quia porro soluta explicabo reiciendis minus nulla, ullam harum esse
+          at reprehenderit doloremque molestiae adipisci sequi inventore
+          voluptatem ex est!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const page = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/image3.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/image3.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/image3.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default page;
